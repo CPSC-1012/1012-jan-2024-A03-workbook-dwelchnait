c87552bbc1194eb4ba8ec0df4e05b0240e1b15c6
Don Welch <dwelch@nait.ca>
2024-01-17 14:50:22 -0700

    First coding programme

---
+++ DemoProgSolution/DemoProgramme/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Don";
+Console.WriteLine($"Hello from {myName}'s World!");
+ myName = "Pat";
+Console.WriteLine($"Hello from {myName}'s World!");
