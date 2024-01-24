ad8b93196bde4768ea61b4fde5ac5cd03d971362
Don Welch <dwelch@nait.ca>
2024-01-24 14:00:06 -0700

    created the program plan

---
+++ SequenceAndArithmetic/CylinderAreaAndVolume/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Cylinder Calculator\n");
+//This program will input a radius and length value for a cylinder then calculate
+//      and display the clyinder are and volume.
+
+//Plan
+//DECLARE inputValue as string
+//DELCARE radius as double
+//DELCARE length as double
+//DECLARE pi as double
+//DECLARE area as double
+//DECLARE volume as double
+
+//Prompt for radius
+//Read input
+//SET radius to input
+//Prompt for length
+//Read input
+//SET length to input
+//SET pi to the Math constant of pi
+
+//Calculate and set area to radius * radius * pi
+//Calculate and set volume to area * length
+
+//DISPLAY area in a statement
+//DISPLAY volume in a statement
