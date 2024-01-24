5782b4252e6887cf2dd0e1ec639dac859ad377cd
Don Welch <dwelch@nait.ca>
2024-01-24 14:36:12 -0700

    coded solution to finding area and volume of a cylinder. Explore the Math class

--- SequenceAndArithmetic/CylinderAreaAndVolume/Program.cs
+++ SequenceAndArithmetic/CylinderAreaAndVolume/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+
 Console.WriteLine("Hello, Cylinder Calculator\n");
 //This program will input a radius and length value for a cylinder then calculate
 //      and display the clyinder are and volume.
@@ -24,3 +26,31 @@ Console.WriteLine("Hello, Cylinder Calculator\n");
 
 //DISPLAY area in a statement
 //DISPLAY volume in a statement
+
+string inputValue;
+double radius = 0.0;
+double length = 0.0;
+double pi = 0.0;
+double area = 0.0, volume = 0.0;
+
+Console.Write("Enter your radius:\t");
+inputValue = Console.ReadLine();
+//convert the strng to the appropriate datatype
+radius = double.Parse(inputValue);
+
+Console.Write("Enter your length:\t");
+inputValue = Console.ReadLine();
+//convert the strng to the appropriate datatype
+length = double.Parse(inputValue);
+
+//introduce you to the Math class
+//the syntax for using items from the Math class is -> Math.classitem
+pi = Math.PI;
+
+//area = radius * radius * pi;
+area = Math.Pow(radius, 2) * pi;
+volume = area * length;
+
+Console.WriteLine($"The area is {area.ToString("#,##0.0000")}"); //auto rounds up
+Console.WriteLine($"The area is {Math.Round(area,4,MidpointRounding.ToNegativeInfinity)}"); //Math class .Round
+Console.WriteLine($"The volume is {volume.ToString("#,##0.0")}");
