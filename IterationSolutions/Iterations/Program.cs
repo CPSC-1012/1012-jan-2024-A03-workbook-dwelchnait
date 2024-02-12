655e5c4cab2da5692e5574591dd1292c6aec6e29
Don Welch <dwelch@nait.ca>
2024-02-12 11:47:26 -0700

    Problem Exercises 1 Question 2 Post-Condition

--- IterationSolutions/Iterations/Program.cs
+++ IterationSolutions/Iterations/Program.cs
@@ -13,24 +13,32 @@ Console.WriteLine("\n\tThis World is loopy!\n");
 string inputValue;
 int numberOfIterations = 0;
 int sumOfSquares = 0;
-bool validFlag = true;
+bool validFlag = false;
 int loopCounter = 0;
 
-//obtain the number of iterations
-Console.Write("Enter a positive number of squares to sum:\t");
-inputValue = Console.ReadLine();
-if (!int.TryParse(inputValue, out numberOfIterations))
+//continue to prompt for a valid number until the user
+//  enters a valid number
+
+while (!validFlag)
 {
-    Console.WriteLine($"Your entry of >{inputValue}< is not a integer number.");
-    validFlag = false;
-}
-else
-{
-    if (numberOfIterations < 0)
+    //obtain the number of iterations
+    Console.Write("Enter a positive number of squares to sum:\t");
+    inputValue = Console.ReadLine();
+    if (!int.TryParse(inputValue, out numberOfIterations))
+    {
+        Console.WriteLine($"Your entry of >{inputValue}< is not a integer number.");
+        validFlag = false;
+    }
+    else if (numberOfIterations < 0)
     {
         Console.WriteLine($"Your entry of >{inputValue}< is a negative integer number.");
         validFlag = false;
     }
+    else
+    {
+        validFlag = true; //will cause the loop to terminate
+    }
+    
 }
 
 if (validFlag)
@@ -40,11 +48,46 @@ if (validFlag)
     //syntax:   while(condition) { ...coding block ...}
     while (loopCounter <= numberOfIterations)
     {
-        sumOfSquares = sumOfSquares + (loopCounter * loopCounter);
+        //sumOfSquares = sumOfSquares + (loopCounter * loopCounter);
+        sumOfSquares += (loopCounter * loopCounter);
+
         //increment the loopCounter to indicate the number of times
         //  one has gone through the loop
-        loopCounter = loopCounter + 1;
+        //loopCounter = loopCounter + 1;
+        //loopCounter += 1;
+        loopCounter ++;  //single unit increment of variable + 1s
+                         //increment after use
+        ++loopCounter;   //increment before use
+
     }
 
     Console.WriteLine($"The sum of squares for {numberOfIterations} is {sumOfSquares}");
 }
+
+
+//Exercise problems 1 question 2
+
+int aNumber = 0;
+int sumOfNumbers = 0;
+int countOfNumbers = 0;
+double avgOfNumbers = 0.0;
+
+//this code needs to be done at least once
+//the loop structure for this solution will use the POST-Test condition
+//syntax:    do{ ...coding block ...} while(condition(s));
+
+do
+{
+        Console.Write("Enter a positive whole number:\t");
+        inputValue = Console.ReadLine();
+        aNumber = int.Parse(inputValue);
+   
+    if (aNumber > 0)        //do not count the exit number as part of the summing
+    {
+        sumOfNumbers += aNumber;
+        countOfNumbers++;
+    }
+} while (aNumber > 0);
+
+avgOfNumbers = sumOfNumbers / countOfNumbers;
+Console.WriteLine($"The average of your {countOfNumbers} number(s) is {avgOfNumbers}");
